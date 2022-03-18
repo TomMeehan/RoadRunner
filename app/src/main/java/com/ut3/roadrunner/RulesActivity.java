adfb9428e80d9615541153b5e98ad0c6f43249fc
PerezGautier <gautier.perez@etud.univ-jfc.fr>
2022-03-18 10:21:12 +0100

    copie des menus du minijeu

---
+++ app/src/main/java/com/ut3/roadrunner/RulesActivity.java
@@ -0,0 +1,27 @@
+package com.ut3.roadrunner;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class RulesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_rules);
+    }
+
+    public void startRulesActivity(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
